ebfeec8f9433b523cacd0ed4103ea461d04bfe1b
mascotguardian <pmaddison@gmail.com>
2019-04-30 04:30:06 +1000

    Implemented contant tag helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
