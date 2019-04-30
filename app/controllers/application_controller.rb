fb356dee492a36cc35285f444c41df113c6d0387
mascotguardian <pmaddison@gmail.com>
2019-05-01 01:14:47 +1000

    Added copyright information in in preparation to build a copyright gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
 
   include DeviseWhitelist
+  include SetSource
   include CurrentUserConcern
   include DefaultPageContent
 
-  before_action :set_source
+  before_action :set_copyright
 
-  def set_source
-    session[:source] = params[:q] if params[:q]
+  def set_copyright
+    @copyright = MaddisonViewTool::Renderer.copyright 'Paul Maddison', 'All rights reserved'
+  end
+end
+
+
+module MaddisonViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
   end
 end
