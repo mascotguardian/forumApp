7f844a33509deb1591f7c377895724300ef5fe6d
mascotguardian <pmaddison@gmail.com>
2019-05-02 10:36:45 +1000

    edited the line re visit from twitter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def source_helper(layout_name)
     if session[:source]
-    greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+    greeting = "Thanks for visiting me from #{session[:source]}. You are on the #{layout_name} layout"
     content_tag(:p, greeting, class: "source-greeting")
     end
   end
